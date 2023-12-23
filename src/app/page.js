7eb71eaf8af74744bdb5185188df649ad63c4fca
tjvitchutripop <khunteeratham@gmail.com>
2023-12-23 01:01:18 -0500

    Added Welcome section

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { Space_Mono, Ubuntu } from 'next/font/google'
 import { useScramble } from 'use-scramble';
 import ShareLocationIcon from '@mui/icons-material/ShareLocation';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import Welcome from './components/Welcome';
 
  
 const space_mono = Space_Mono({ weight: '400', subsets: ['latin']})
@@ -22,29 +23,31 @@ export default function Home() {
     overdrive: false
   });
   return (
-    <main>
+    <main style={{overflow:"hidden", marginBottom:50}}>
       <Navbar />
-      <div style={{ position: 'relative', width: '100vw', height: '95vh' }}>
+      <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
         <Image
           src="/assets/main_landing.png"
           alt="Landing Page"
           layout="fill"
           objectFit="cover"
+          priority={true}
         />
       </div>
-      <div className="overlayText" >
-      <h1 className={space_mono.className} style={{fontWeight:"Bold"}} ref={ref}/>
-      <div className="overlayButtons">
-        <div style={{display:"flex", fontSize:30, textAlign:"left"}}>
-        <h2 className={ubuntu.className} ><CalendarMonthIcon style={{marginTop:-5}}/> March 23-24</h2>
-        <h2 className={ubuntu.className} style={{marginLeft:20}}><ShareLocationIcon style={{marginTop:-5, marginRight:5}}/>University of Virginia</h2>
+      <div className="overlayText">
+        <h1 className={space_mono.className} style={{fontWeight:"Bold", marginTop:25}} ref={ref}/>
+        <div className="overlayButtons">
+          <div className="dateLocation">
+          <h2 className={ubuntu.className} ><CalendarMonthIcon style={{marginTop:-5}}/> March 23-24</h2>
+          <h2 className={ubuntu.className}><ShareLocationIcon style={{marginTop:-5, marginRight:5}}/>University of Virginia</h2>
+        </div>
+      <div>
+      <button className="bg-orange-400 hover:bg-gray-500 text-white font-bold py-3 px-10 text-l rounded-full">Register</button>
+      <button className="bg-orange-400 hover:bg-gray-500 text-white font-bold py-3 px-10 text-l rounded-full" style={{ marginLeft: "30px", marginTop:"10px" }}>Sponsor</button>
       </div>
-      <div style={{textAlign:"left"}}>
-      <button className="bg-orange-400 hover:bg-gray-500 text-white font-bold py-3 px-10 text-xl rounded-full">Register</button>
-      <button className="bg-orange-400 hover:bg-gray-500 text-white font-bold py-3 px-10 text-xl rounded-full" style={{ marginLeft: "30px", marginTop:"10px" }}>Sponsor</button>
       </div>
       </div>
-      </div>
+      <Welcome />
      
     </main>
   );
