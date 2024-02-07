3c8b487820cec70d158c8861349a1881a9cd4c3e
tjvitchutripop <khunteeratham@gmail.com>
2024-02-07 16:57:17 -0500

    Redirecting register

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
-    }
+    },
+    async redirects() {
+        return [
+          {
+            source: '/register',
+            destination: 'https://app.hoohacks.io/',
+            permanent: true,
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
