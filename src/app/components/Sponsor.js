9208d6c259c88be68d489017a9a18da6b41cd63b
tjvitchutripop <khunteeratham@gmail.com>
2024-03-06 00:12:05 -0500

    refreshed sponsors + partners page

--- src/app/components/Sponsor.js
+++ src/app/components/Sponsor.js
@@ -21,16 +21,29 @@ export default function Sponsor() {
     playOnMount: false,
   });
 
+  const { ref: textRef2, replay: replay2 } = useScramble({
+    text: "OUR PARTNERS",
+    speed: 0.5,
+    tick: 1,
+    step: 1,
+    scramble: 5,
+    overdrive: false,
+    playOnMount: false,
+  });
+
   useEffect(() => {
     if (isInView) {
       replay();
+      replay2();
     }
   }, [isInView]);
 
   return (
     <div className="sponsorContainer" ref={ref} id="sponsor">
       <h2 className={ubuntu.className + " welcomeTitle"} ref={textRef} />
-      <img src="/assets/sponsors.svg" alt="Hoohacks 2024 Sponsors" />
+      <img src="/assets/sponsors.png" alt="Hoohacks 2024 Sponsors" />
+      <h2 style={{marginTop: 40}}className={ubuntu.className + " welcomeTitle"} ref={textRef2} />
+      <img src="/assets/partners.png" alt="Hoohacks 2024 Partners" />
     </div>
   );
 }
