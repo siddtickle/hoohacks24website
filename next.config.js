d005d7fb0c5df5255a583bcf7760a2904f57f306
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2024-03-19 19:44:00 -0400

    added /discord endpoint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-}
+  //   output: "export",
+  //   images: {
+  //     unoptimized: true,
+  //   },
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/BzDGFTAbQe",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
